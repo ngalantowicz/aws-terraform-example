40b55ffddf65fbdf022d26c25284be58ba088456
ngalantowicz <ngalantowicz@gmail.com>
2017-07-11 09:31:52 -0400

    added documentation to main.tf file

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
+######################################################
+# Tells terraform I'm using AWS and want to provision
+# the below resources in the region stated
 provider "aws" {
   region     = "us-west-2"
 }
 
-resource "aws_instance" "dicky-instance" {
+#####################################################
+# Generates ec2 resource with desired services
+resource "aws_instance" "example-instance" {
   count                        = 1
   ami                          = "ami-4836a428"
   instance_type                = "t2.micro"
@@ -10,12 +15,15 @@ resource "aws_instance" "dicky-instance" {
   key_name                     = "your_personal_key"
 
   tags {
-    Name = "dicky-test"
+    Name = "example-instance"
   }
 }
 
+###################################################################
+# Provisions new security group specific for the above EC2, which
+# allows ssh on port 22, http on port 80 and outbond traffic on all ports
 resource "aws_security_group" "instance" {
-  name = "dicky-test-instance"
+  name = "example-instance"
 
   ingress {
     from_port   = 22
@@ -47,14 +55,20 @@ resource "aws_security_group_rule" "http" {
   cidr_blocks = ["0.0.0.0/0"]
 }
 
+##################################################################################################
+# The null_resource below provisions not an explicit AWS service but rather extra actions to run
+# when I execute this main.tf file.  The first provisioner attaches the newly created EC2's IP into
+# my ansible inventory.ini file.  The second provisioner executes the ansible playbook that
+# the copys the html dir into the new EC2 server, installs NGINX, points NGINX to the html dir,
+# and starts NGINX
 resource "null_resource" "jrwebdev" {
 
   triggers {
-    cluster_instance_ids = "${aws_instance.dicky-instance.id}"
+    cluster_instance_ids = "${aws_instance.example-instance.id}"
   }
 
   provisioner "local-exec" {
-    command = "sed -i -e '2s/.*/${aws_instance.dicky-instance.public_ip}/' deploy-jrwebdev/inventory.ini"
+    command = "sed -i -e '2s/.*/${aws_instance.example-instance.public_ip}/' deploy-jrwebdev/inventory.ini"
   }
 
   provisioner "local-exec" {
